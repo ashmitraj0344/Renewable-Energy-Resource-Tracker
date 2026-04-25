79654b2803eacf8119c70b5ae9f0d7dc2b9e5ac2
ASHMIT RAJ <ashmitr0344@gmail.com>
2026-04-25 12:57:47 +0530

    Clean Interface

--- check_dbs.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-async function run() {
-  await mongoose.connect('mongodb://localhost:27017');
-  const admin = mongoose.connection.db.admin();
-  const dbs = await admin.listDatabases();
-  console.log(JSON.stringify(dbs.databases, null, 2));
-  process.exit(0);
-}
-run().catch(console.error);
